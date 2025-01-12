d356ee1771dd16be072468ac0b6c4d6f7babd41a
punk <punk@beelinerouter.net>
2025-01-13 02:07:30 +0300

    aboba

--- lua/community.lua
+++ lua/community.lua
@@ -5,11 +5,9 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.colorscheme.oxocarbon-nvim" },
   { import = "astrocommunity.completion.codeium-vim" },
   { import = "astrocommunity.lsp.lsp-signature-nvim" },
   { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.media.image-nvim" },
   -- import/override with your plugins folder
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -10,8 +8,7 @@ return {
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
-        "lua_ls",
-        -- add more arguments for adding more language servers
+        "ts_ls",
       },
     },
   },
@@ -21,7 +18,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
-        "stylua",
+        "prettier",
         -- add more arguments for adding more null-ls sources
       },
     },
@@ -31,7 +28,6 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
       ensure_installed = {
-        "python",
         -- add more arguments for adding more debuggers
       },
     },
